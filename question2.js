f1e97c3e51d09c12032f38b5d6496eac588cc391
filippanorell <145430464+filippanorell@users.noreply.github.com>
2023-11-27 11:56:18 +0100

    first commit

---
+++ question2.js
@@ -0,0 +1,10 @@
+// 2. String-arrays (3p)
+// Skriv klart funktionen filterWordsWithLetterA som tar en array med strängar som
+// input och returnerar en array med bara de strängar som innehåller "a" (bara liten bokstav).
+// tips: använd indexOf eller includes
+
+function filterWordsWithLetterA(words) {
+  return words.filter(word => word.includes('a'));
+}
+
+console.log(filterWordsWithLetterA(["Programming", "is", "great!"])); // ska logga ["Programming", "great!"]
